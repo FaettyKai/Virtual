a0ef717e06675c1725937f12bf1178e9bf08b8fd
Michael <140651581+FaettyKai@users.noreply.github.com>
2023-11-21 00:01:33 -0600

    Button context now pulsates; not the button. Added hover for the nav bar along with more HTML.

--- script.js
+++ script.js
@@ -11,23 +11,40 @@ files = document.querySelector(".files")
 // COMMENT: This code makes the button pulse for about 5 seconds (alternating opacity every second) after it's clicked.
 //Add aria-label to help set context for user.
 
+// Updated to take the span including the buttons context. Context now pulsates; not the button.
 button.addEventListener("click", () => {
-   button.textContent = ".";
-   let counter = 0;
-
-   let intervalId = setInterval(() => {
-       let isEven = counter % 2 === 0;
-       button.style.transition = "opacity 1s";
-       button.style.opacity = isEven ? "1" : "0";
-       counter++;
-       
-       if(counter > 7){
-           clearInterval(intervalId);
-           button.textContent = "ERROR: DATA BREACH";
-           button.setAttribute("style", "color: red;")
-           button.style.opacity = "1"
-       }
-   }, 1000);
-});
+    buttonSpan.textContent = ".";
+    let counter = 0;
+ 
+    let intervalId = setInterval(() => {
+        let isEven = counter % 2 === 0;
+        buttonSpan.style.transition = "opacity 0.5s";
+        buttonSpan.style.opacity = isEven ? "1" : "0";
+        counter++;
+ 
+        if(counter > 8){
+            clearInterval(intervalId);
+            buttonSpan.textContent = "401 Unauthorized";
+            button.setAttribute("style", "color: red;")
+            button.style.opacity = "1"
+            buttonClick = true;
+                 reveal()
+        }
+    }, 1000);
+ });
+ 
+ // reaveal candidate & sign-up upon click of button-text
+ let buttonClick = false; 
+ 
+ let reveal = () => {
+     if(buttonClick === true) {
+       candidate.setAttribute("style", "opacity: 1; opacity: transition: 0.2s;")
+     signUp.setAttribute("style", "opacity: 1; opacity: transition: 0.2s")
+   }
+ };
+ 
+ home.addEventListener("click", () => {
+ })
+ 
 
 
